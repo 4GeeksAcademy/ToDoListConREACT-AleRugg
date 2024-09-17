6a53c422be6643512c60c66638b77ed89a1b6dda
Alerugg <134484194+Alerugg@users.noreply.github.com>
2024-09-17 00:17:55 +0000

    agregadas las alertas de caracteres y de maximos articulos en la lista con useEffect y funcionando bien queda pte la estetica y posicion de las alertas

--- src/js/ToDoList.jsx
+++ src/js/ToDoList.jsx
@@ -2,22 +2,14 @@ import React, { useState, useEffect } from "react";
 import { BsFillEraserFill } from "react-icons/bs";
 
 
-
-
-
-
 const ToDoList = () => {
 
     const [tasks, setTasks] = useState([])
     const [inputValue, setInputValue] = useState("")
+    const [error, setError] = useState(false)
+    const [error2, setError2] = useState(false)
 
-
-
-const alerts = () => {
-    if(tasks.length >= 10) {
-       console.log( alert("Listo"))
-    }
-}
+   
 
 
     const inputHandler = (e) => {
@@ -27,17 +19,34 @@ const alerts = () => {
 
     const handlerKeyDown = (e) => {
 
-        if (e.key === "Enter" && inputValue.trim() !== "" ) {
+        if (e.key === "Enter" && inputValue.trim() !== "") {
 
             setTasks([...tasks, inputValue])
             setInputValue(""); // Limpia el input
-
         }
 
 
     }
 
+    useEffect(()=> {
+        if(tasks.length == 10) {
+            setError(true)
+            console.log("funciona") // con este hacemos una alerta  de lmite de 10 tareas
+        }
 
+        else setError(false)
+    })
+
+    let maxLength = 100;
+
+    useEffect(() => {
+        if( inputValue.length == maxLength) {
+            setError2(true)
+        }
+        else {setError2(false)}
+        
+         // copn este hacemos una alerta de caracteres maximos dentro del input
+    })
 
     return (
 
@@ -52,9 +61,11 @@ const alerts = () => {
                         onChange={inputHandler}
                         onKeyDown={handlerKeyDown}
                         disabled={tasks.length >= 10}
-                       
-                       
+                        maxLength={100}
+                          
                     />
+                    { error && <span className="bg-danger">"Hola funciono como alerta para los 10 li"</span>}
+                    { error2 && <span className="bg-danger">"Hola funciono como alerta para los caracteres"</span>}
 
                 </div>
                 <h4 className="title">Lista de tareas por hacer:</h4>
@@ -64,23 +75,24 @@ const alerts = () => {
                         {tasks.map((li, index) => {
 
                             return (
-                                
-                                    <li className="text-white liGenerado"                                        
-                                        value={li.value}
-                                        key={index}
-                                        
-                                     
-                                    >
-                                        {li} <div>     <BsFillEraserFill className="eraserIcon" /></div>
-                                    </li>
-                                
+
+                                <li className="text-white liGenerado"
+                                    value={li.value}
+                                    key={index}
+                                    
+                                >
+                                    {li}                                    
+                                        <BsFillEraserFill className="eraserIcon" />                                    
+                                </li>
+
                             )
                         })}
                     </ul>
-                    <div className="card.footer text-white">Llevas {tasks.length} / 10 tareas agregadas a tu lista.</div>
+                    <div className="card.footer text-white footer">Llevas {tasks.length} / 10 tareas agregadas a tu lista.</div>
                 </div>
             </div>
         </div>
-    )}
+    )
+}
 
 export { ToDoList }
